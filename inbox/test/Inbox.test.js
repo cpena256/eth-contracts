657f944f716341b2a6ba10fd0c8a9a7e6e4a2716
Chris Pena <nothinbuthits512@gmail.com>
2021-04-02 13:42:27 -0400

    Install Ganache and web3 modules

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //Constructor function/ class
+const web3 = require('web3'); //Our actual instance of Web3
+//Web 3 allows JavaScript communicate with the Ethereum network.
+
