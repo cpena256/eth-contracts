9bc44e290f1537864775c749c61514de8b7d3e02
Chris Pena <nothinbuthits512@gmail.com>
2021-04-02 15:18:32 -0400

    mocha test setup

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3'); //Constructor function/ class
-const web3 = require('web3'); //Our actual instance of Web3
+const Web3 = require('web3'); //Constructor function/ class 
+const web3 = new Web3(ganache.provider()); //Our actual instance of Web3
 //Web 3 allows JavaScript communicate with the Ethereum network.
+
 
