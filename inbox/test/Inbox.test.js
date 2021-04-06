964d9f37eafbd266a4ed710ecdc653d016e341e3
Chris Pena <nothinbuthits512@gmail.com>
2021-04-06 08:42:47 -0400

    End of testing for now

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -9,13 +9,14 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+
 beforeEach(async () => {
     // Get a list of all accounts. This is asyncronous which is the reason for await.
     accounts = await web3.eth.getAccounts();
     //Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract (JSON.parse(interface))
     .deploy({data: bytecode, arguments: ['Hi there!']})
-    .send({ from: accounts[0], gas: 1000000});
+    .send({ from: accounts[0], gas: '1000000'});
 
     inbox.setProvider(provider);
 
@@ -24,5 +25,16 @@ beforeEach(async () => {
 describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+         const message = await inbox.methods.message().call();
+         assert.strictEqual(message, 'bye');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({from: accounts[0]})
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'bye');
     });
 });
